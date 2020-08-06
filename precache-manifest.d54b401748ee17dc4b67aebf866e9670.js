a9e9b17ed05996e875f67c097778c539e9224606
Stephen Johnson <jjohns55@g.uafs.edu>
2020-08-06 17:12:52 -0500

    Updates

---
+++ precache-manifest.d54b401748ee17dc4b67aebf866e9670.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3fbd5f6c9bd5addcc16e2bb7b3462f",
+    "url": "/jjohns55/finalReact/index.html"
+  },
+  {
+    "revision": "1eece7c5becd6a46c90f",
+    "url": "/jjohns55/finalReact/static/css/main.0b9e1365.chunk.css"
+  },
+  {
+    "revision": "216381cb3ad6be8d6ca8",
+    "url": "/jjohns55/finalReact/static/js/2.b35f68bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jjohns55/finalReact/static/js/2.b35f68bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eece7c5becd6a46c90f",
+    "url": "/jjohns55/finalReact/static/js/main.ef2a50f2.chunk.js"
+  },
+  {
+    "revision": "8d801acd1fcf94abb946",
+    "url": "/jjohns55/finalReact/static/js/runtime-main.326b5c37.js"
+  },
+  {
+    "revision": "88e1a5d638240bf7022c9c116b9b4e28",
+    "url": "/jjohns55/finalReact/static/media/Torch_Gif.88e1a5d6.gif"
+  },
+  {
+    "revision": "07a9783428ed8e7f4b552a2d557b5f67",
+    "url": "/jjohns55/finalReact/static/media/campfire.07a97834.gif"
+  }
+]);
